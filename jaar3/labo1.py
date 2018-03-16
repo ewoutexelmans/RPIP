0131213f27808d8711ef36539790bde1d03c0e91
ewoutexelmans <ewoutexelmans>
2018-03-16 11:58:38 +0000

    bugfixes labo1

--- jaar3/labo1.py
+++ jaar3/labo1.py
@@ -9,17 +9,18 @@ button = [17, 27, 22]
 
 index = 0
 
-guess = []
+guess = [0, 0, 0, 0, 0]
 key = [1, 2, 3, 1, 2]
 
 burn = False
 
 GPIO.setup(led, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.output(led, burn)
+GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 def print_name(x, index):
     print("Button {} pressed".format(x))
-    guess.insert(x, index)
+    guess[index] = x
     if guess == key:
         print("You guessed correctly")
         return True
@@ -29,21 +30,23 @@ def print_name(x, index):
 try:
     print("Press the buttons in the correct sequence to make the led burn")
     while not burn:
-
-        if count > 4:
+        if index > 4:
+            print("Wrong sequence, Try again.")
             index = 0
-
-        if GPIO.input(button[0]):
+        a = GPIO.input(button[0])
+        b = GPIO.input(button[1])
+        c = GPIO.input(button[2])
+        if a == False:
             x = 1
-            burn = print_name(x)
+            burn = print_name(x, index)
             index += 1
-        elif GPIO.input(button[1]):
+        elif b == False:
             x = 2
-            burn = print_name(x)
+            burn = print_name(x, index)
             index += 1
-        elif GPIO.input(button[2]):
+        elif c == False:
             x = 3
-            burn = print_name(x)
+            burn = print_name(x, index)
             index += 1
         time.sleep(0.3)
     else:
