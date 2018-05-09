07600fb1a2719ddb3b88478648c8dff691ce6d39
ewoutexelmans <ewoutexelmans>
2018-05-09 14:18:20 +0000

    split labo4.2

---
+++ jaar3/labo4.2a.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import readchar
+
+GPIO.setmode(GPIO.BCM)
+
+pin1 = 12
+
+freq = 100
+dc = 10
+
+GPIO.setup(pin1, GPIO.OUT)
+
+pwm1 = GPIO.PWM(pin1,freq)
+pwm1.start(dc)
+
+
+while readchar.readkey() != readchar.key.ENTER:
+    if readchar.readkey()=='a':
+        freq = 100
+        print(freq)
+    if readchar.readkey()=='z':
+        freq = 1000
+        print(freq)
+    if readchar.readkey()=='e':
+        freq = 5000
+        print(freq)
+    if readchar.readkey()=='r':
+        freq = 10000
+        print(freq)
+    if readchar.readkey()=='q':
+        dc = 10
+        print(dc)
+    if readchar.readkey()=='s':
+        dc = 50
+        print(dc)
+
+    pwm1.ChangeDutyCycle(dc)
+    pwm1.ChangeFrequency(freq)
+
+pwm1.stop()
+GPIO.cleanup()
+print("Cleanup and shutting down program")
